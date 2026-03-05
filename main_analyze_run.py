41b7d3115b05310de63f91f45e99cd14396264dc
Pho Hale <PhoHale@gmail.com>
2026-03-04 21:52:41 -0500

    fixup imports

--- main_analyze_run.py
+++ main_analyze_run.py
@@ -1390,10 +1390,10 @@ def process_XDFs_main(n_most_recent_sessions_to_preprocess: Optional[int] = 5,
 
 if __name__ == "__main__":
 
-    # n_most_recent_sessions_to_preprocess: int = None # None means all sessions
+    n_most_recent_sessions_to_preprocess: int = None # None means all sessions
     # n_most_recent_sessions_to_preprocess: int = 55
     # n_most_recent_sessions_to_preprocess: int = 15
-    n_most_recent_sessions_to_preprocess: int = 5
+    # n_most_recent_sessions_to_preprocess: int = 5
 
     should_load_preprocessed: bool = False
     # should_load_preprocessed: bool = True
